c34bc2e363371c4b2feaf12dc0a3c285796e462f
Adil Altaf <khanadilaltaf@gmail.com>
2022-09-21 11:46:48 -0400

    Created Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <Box
+      as="section"
+      bg="#6B46C1"
+      color="#F7FAFC"
+      pt="90px"
+      pb="198px"
+      px="32px"
+    >
+      <Heading fontWeight="800" fontSize="48px">
+        Simple pricing for your business
+      </Heading>
+      <Text fontWeight="500" fontSize="24px" pt="16px">
+        Plans that are carefully crafted to suit your business.
+      </Text>
+    </Box>
+  );
+};
